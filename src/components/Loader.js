7b584faa5f3e2477961e08f4279b2fa6a9067023
nasim khalili <nasimkhalili1385nasim@gmail.com>
2022-08-31 15:29:16 +0430

    Coin structure and design

---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Gif
+import spinner from "../gif/spinner.gif"
+
+const Loader = () => {
+    return (
+        <div>
+            <img src={spinner} alt="Loading" />
+        </div>
+    );
+};
+
+export default Loader;
